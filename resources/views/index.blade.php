1347dbbaaab4a30462ed1c4c8eb158d3016d6591
take52 <58460151+takeshimakai52@users.noreply.github.com>
2020-03-13 16:42:16 +0900

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+@extends('template')
+
+
+@section('content')
+<div class="container content-inside">
+  <div class="row">
+    <div class="greet_start mt-3">
+      <h2>mezase!</h2>
+      <h2>goodkomyunike-syonn</h2>
+      <a class="start_btn btn mt-2" href="#" role="button">会話を始める</a>
+    </div>
+  </div>
+  <p class="description mt-3">gosetumei</p>
+  <div class="row">
+    <div class="description_content col-md-6">
+      一問一答形式の会話を成立させコミュニケーション能力の向上を図ります。
+      選択肢は3つ、グッドコミュニケーションなものを選んでください。
+    </div>
+    <div class="description_content col-md-6" style="text-align: center;">
+      <img src="/images/top.jpg" alt="" style="margin: 0 auto">
+    </div>
+  </div>
+  <p class="description mt-3">osirase</p>
+  <!-- <div class="top mx-auto" style="width: auto;height:300px;"> -->
+    
+  </div>
+
+
+</div>
+
+
+@endsection
---
+++ resources/views/template.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  {{-- CSRF トークン --}}
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>@if (! Request::is('/')){{ $title }} | @endif{{ env('APP_NAME') }}</title>
+
+  {{-- CSS --}}
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+  <nav class="navbar navbar-expand-lg navbar-dark sticky-top"style="background-color:#F9AA8F;">
+    <div class="container">
+      <a class="navbar-brand" href="{{ url('/') }}">
+          {{ config('app.name') }}
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" 
+      style="border-color: #ffffff;">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        {{-- Navbarの左側 --}}
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('posts') }}">{{ __('Posts') }}</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('users') }}">{{ __('Users') }}</a>
+            </li>
+        </ul>
+
+          {{-- Navbarの右側 --}}
+        <!-- <ul class="navbar-nav ml-auto">
+          {{-- 投稿ボタン --}}
+          <li class="nav-item">
+            <a href="{{ url('posts/create') }}" id="new-post" class="btn btn-success">
+              {{ __('New Post') }}
+            </a>
+          </li> 
+        </ul> -->
+      </div>
+    </div>
+  </nav>
+</div>
+<main class="main">
+    @yield('content')
+</main>
+</div>
+<footer class="footer">
+  <div class="container">
+    <p class="text-muted" style="margin-top: 0!important;margin-bottom: 0rem!important;">
+    Place sticky footer content here.</p>
+  </div>
+</footer>
+{{-- JavaScript --}}
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
